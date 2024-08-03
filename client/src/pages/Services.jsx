73d693fbcdd3873a9aae5982ac00a1b782a47456
Mehnaz-1 <mehnaxr@gmail.com>
2024-08-03 19:16:59 +0500

    it change

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -3,12 +3,43 @@ import React from 'react'
 export default function Services() {
     return (
         <>
-            <div>
-                <h1>
+     
+         
+            <div className='bg-green-700 h-lvh w-full bg-cover bg-center p-24'>
+                <img src="./shabu2.jpeg" alt="" />
+                <h1 className='text-white text-6xl font-bold ml-10'>Welcome to HomeEase <br />
                     Services
-                    asdaksjdlas
                 </h1>
-            </div >
+                <h4 className='text-white text-3xl font-light mt-5 ml-10'>Discover a wide range of household maintenance services tailored to your needs.</h4>
+
+                <br /> <br /> <br /> <br /> <br /> <br /> <br />
+
+                <div class="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 mx-auto text-center mt-9 md:gap-4 sm:gap-6 text-white mr-96">
+                    <div class="text-center gap-40">
+                        <h2 class="text-xl md:text-2xl font-bold">10+</h2>
+                        <p>Service Categories</p>
+                    </div>
+                    <div class="text-center">
+                        <h2 class="text-xl md:text-2xl font-bold">500+</h2>
+                        <p>Trusted Professionals</p>
+                    </div>
+                    <div class="text-center">
+                        <h2 class="text-xl md:text-2xl font-bold">10,000+</h2>
+                        <p>Completed Projects</p>
+                    </div>
+                    <div class="text-center">
+                        <h2 class="text-xl md:text-2xl font-bold">98%</h2>
+                        <p>Customer Satisfaction</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
 
         </>
     )
